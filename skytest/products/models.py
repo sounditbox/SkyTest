d2fe0848b8e83de9457ddbfee2578cc346cd7b00
Ruslan <sounditbox@gmail.com>
2025-03-20 16:31:33 +0400

    Done models

--- skytest/products/models.py
+++ skytest/products/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+from django.db.models import Model, CharField, CASCADE, BooleanField, \
+    DecimalField, ForeignKey, DateField, PositiveIntegerField
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(Model):
+    name = CharField("Название категории", max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(Model):
+    name = CharField("Название товара", max_length=255)
+    category = ForeignKey(Category, on_delete=CASCADE, verbose_name="Категория")
+    is_active = BooleanField("Активен", default=True)
+    price = DecimalField("Цена", max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(Model):
+    product = ForeignKey(Product, on_delete=CASCADE, verbose_name="Товар")
+    order_date = DateField("Дата заказа", default=timezone.now)
+    quantity = PositiveIntegerField("Количество", default=1)
+
+    def __str__(self):
+        return f"Заказ для {self.product.name} от {self.order_date}"
