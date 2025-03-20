cc58dc06a3e9ab7881b76744cc0069c16d9cca81
Ruslan <sounditbox@gmail.com>
2025-03-20 17:55:54 +0400

    basic admin page

--- skytest/products/admin.py
+++ skytest/products/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product, Order
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "category", "is_active", "price")
+    list_filter = ("is_active", "category")
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "product", "order_date", "quantity")
+    list_filter = ("order_date",)
