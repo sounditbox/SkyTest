de2bbb2961fdc588a7dfcb3fb5626f8af9f215ce
Ruslan <sounditbox@gmail.com>
2025-03-21 00:54:01 +0400

    Added autocreate of superuser

--- skytest/entrypoint.sh
+++ skytest/entrypoint.sh
@@ -5,4 +5,17 @@ python manage.py migrate --noinput
 
 python manage.py collectstatic --noinput
 
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+import os
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(
+        username=username,
+        email=os.environ.get('DJANGO_SUPERUSER_EMAIL'),
+        password=os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+    )
+EOF
+
 exec gunicorn skytest.wsgi:application --bind 0.0.0.0:8000
